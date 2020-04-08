d6f545c5e755c28a78375f56c410c7a749812a66
blumbergleer <63240060+blumbergleer@users.noreply.github.com>
2020-04-08 07:37:13 -0500

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# LeeRepository
-Created with CodeSandbox
---
+++ src/index.js
@@ -0,0 +1,15 @@
+// /*
+// 🚨Game Overview🚨
+
+// - The game is played in the browser
+// - The user is asked their name via the `prompt()` function. Their name is stored for later use.
+// - Questions and answers are stored in variables (only basic types are used).
+// - The user's score is initially zero. If they get the the answer correct, the score increases.
+// - The user submits their answers via the `prompt()` function.
+// - At the end of each answer, the score will either increase or stay the same, and the next question will appear.
+// - Once the user has answered all of the questions, an `alert()` function will trigger, letting them know their score given in the following format:
+
+// - **0-2** "Oh wow {name}, you can do better."
+// - **3-4** "Keep practicing {name}, you got this!"
+// - **5** "Wow {name}, a perfect score!"
+// */
